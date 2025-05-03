b30d3b541273174e544d5608417411002732d075
you7n <qileniao@yeah.net>
2025-05-03 10:08:31 +0800

    more format practice

--- lib/profilePage.dart
+++ lib/profilePage.dart
@@ -6,77 +6,80 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              color: Colors.red[900],
-              child: Center(
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage("images/sheep.jpeg"),
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.red[900],
+                child: Center(
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage("images/sheep.jpeg"),
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 4,
-            child: Container(
-              color: Colors.red,
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                children: [
-                  Expanded(child: Container()),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: Container(
-                        width: 70,
-                        height: 70,
-                        color: Colors.green,
+            Expanded(
+              flex: 4,
+              child: Container(
+                color: Colors.red,
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Expanded(child: Container()),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          width: 70,
+                          height: 70,
+                          color: Colors.green,
+                        ),
                       ),
                     ),
-                  ),
-                  Expanded(child: Container()),
-                  Expanded(
-                    child: Container(
-                      color: Colors.blue,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              color: Colors.yellow,
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      color: Colors.indigo,
-                    ),
-                  ),
-                  Expanded(child: Container()),
-                  Expanded(child: Container()),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
+                    Expanded(child: Container()),
+                    Expanded(
                       child: Container(
-                        width: 70,
-                        height: 70,
-                        color: Colors.purple,
+                        color: Colors.blue,
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.yellow,
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Colors.indigo,
+                      ),
+                    ),
+                    Expanded(child: Container()),
+                    Expanded(child: Container()),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          width: 70,
+                          height: 70,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
